5d98861b6bd158a5d668a08dabc0c321832ec7b0
digudigigowda <diganthgowda6408@gmail.com>
2026-08-11 21:10:01 +0530

    python-loops

---
+++ 7941-loops-Hackerrank/7941-loops.py
@@ -0,0 +1,9 @@
+n=int(input())
+if 1<=n<=20:
+    for i in range (n):
+        print(i**2)
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
