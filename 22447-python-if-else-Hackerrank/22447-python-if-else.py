6093143e9bea09ba41544fb97cb08c461fabdc78
digudigigowda <diganthgowda6408@gmail.com>
2026-08-12 18:04:33 +0530

    solution

---
+++ 22447-python-if-else-Hackerrank/22447-python-if-else.py
@@ -0,0 +1,17 @@
+n=int(input())
+if 1<=n<=100:
+    if n%2==0:
+        if n<=20:
+            if 2<=n<=5:
+                print("Not Weird")
+            else:
+                print("Weird")
+        else:
+            print("Not Weird")
+    else:       
+        print("Weird")  
+
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
