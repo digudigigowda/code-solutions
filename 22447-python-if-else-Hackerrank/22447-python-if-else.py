5b8f02bb5822b9b619f7a77b11f4dbd45e30906a
digudigigowda <diganthgowda6408@gmail.com>
2026-08-12 18:06:29 +0530

    updated solution

--- 22447-python-if-else-Hackerrank/22447-python-if-else.py
+++ 22447-python-if-else-Hackerrank/22447-python-if-else.py
@@ -1,15 +1,13 @@
-n=int(input())
-if 1<=n<=100:
-    if n%2==0:
-        if n<=20:
-            if 2<=n<=5:
-                print("Not Weird")
-            else:
-                print("Weird")
-        else:
-            print("Not Weird")
-    else:       
-        print("Weird")  
+n = int(input().strip())
+
+if n % 2 != 0:
+    print("Weird")
+elif 2 <= n <= 5:
+    print("Not Weird")
+elif 6 <= n <= 20:
+    print("Weird")
+else:
+    print("Not Weird")
 
 
 # Synced seamlessly with LeetHub Pro
