25960f02ae44279cc03cebce93dc3507b97788a0
digudigigowda <diganthgowda6408@gmail.com>
2026-08-15 20:17:16 +0530

    solution

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,5 +1,7 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        if len(s)%2!=0:
+            return False
         while "()" in s or "[]" in s or "{}" in s:
             s=s.replace("()","").replace("[]","").replace("{}","") 
         return s==""
