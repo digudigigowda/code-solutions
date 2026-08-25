37f85ffc362dea12852eb02bde7b5988563f24fc
digudigigowda <diganthgowda6408@gmail.com>
2026-08-25 21:41:33 +0530

    Time: 3 ms (97.72%), Space: 32.1 MB (89.96%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, l: List[int]) -> bool:
+        return len(l) != len(set(l))
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
