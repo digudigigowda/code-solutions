40de22e029afdadca557bfbf05e566aeec165686
digudigigowda <diganthgowda6408@gmail.com>
2026-09-02 20:50:00 +0530

    Time: 2 ms (27.96%), Space: 19.3 MB (26.18%) - LeetHub - Time: O(len(t)), Space: O(1)

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i, j = 0, 0
+
+        while i < len(s) and j < len(t):
+            if s[i] == t[j]:
+                i += 1
+            j += 1
+
+        return i == len(s) 
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
