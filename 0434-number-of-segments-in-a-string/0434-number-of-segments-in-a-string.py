3ff583ed2c32135675c1bb57f414b956e7fc28f2
digudigigowda <diganthgowda6408@gmail.com>
2026-09-04 19:28:29 +0530

    Time: 0 ms (100%), Space: 19.4 MB (13.85%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.py
@@ -0,0 +1,8 @@
+class Solution:
+    def countSegments(self, s: str) -> int:
+        return len(s.split())
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
